e6f9b82622428fc7dcdd7ec19114722617a157ae
ViniciusGR797 <loritoamarelo@outlook.com>
2025-10-04 17:49:31 -0300

    feat: add DeduplicationId in sqs

--- app/services/messages.py
+++ app/services/messages.py
@@ -5,13 +5,18 @@ from utils.config import Config
 
 class MessageService:
     @staticmethod
-    def send_to_queue(message: MessageSchema,
-                      queue_url: str = Config.SQS_MAIN_QUEUE):
+    def send_to_queue(
+        message: MessageSchema,
+        queue_url: str = Config.SQS_MAIN_QUEUE,
+        message_group_id: str = "default-group"
+    ):
         try:
             sqs_client = boto3.client("sqs", region_name=Config.REGION)
             sqs_client.send_message(
                 QueueUrl=queue_url,
-                MessageBody=message.model_dump_json()
+                MessageBody=message.model_dump_json(),
+                MessageGroupId=message_group_id,
+                DeduplicationId=message.message_id
             )
             return None
         except Exception as e:
