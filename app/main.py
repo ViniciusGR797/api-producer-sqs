d513d2f5c7102d484b5ea6f82d4efaafc09a7b4c
ViniciusGR797 <loritoamarelo@outlook.com>
2025-10-04 18:14:08 -0300

    fix: root path docs swagger

--- app/main.py
+++ app/main.py
@@ -3,7 +3,13 @@ from fastapi import FastAPI
 from mangum import Mangum
 from routes import users, messages
 
-app = FastAPI(title="SQS API Lambda")
+app = FastAPI(
+    title="SQS API Lambda",
+    root_path="",
+    docs_url="/docs",
+    redoc_url="/redoc",
+    openapi_url="/openapi.json"
+)
 
 app.include_router(users.router, prefix="/users", tags=["Users"])
 app.include_router(messages.router, prefix="/messages", tags=["Messages"])
